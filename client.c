aebb4c563ee825f1199c2573082a9e92168b5f6b
String-C <1642817080@qq.com>
2024-05-30 21:00:11 +0800

    commit code

--- client.c
+++ client.c
@@ -1,53 +1,72 @@
-/*client.c*/
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#define PORT 12345
 
-#define port 4321
-#define BUFFER_SIZE 1024
+void sendCommand(const char *command) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\nSocket creation error\n");
+        return;
+    }
 
-int main(int argc, char *argv[]){
-	int sockfd,sendbytes;
-	char buf[BUFFER_SIZE];
-	struct hostent *host;
-	struct sockaddr_in serv_addr;
-	 
-	if (argc < 3){
-		fprintf(stderr,"USAGE: ./clientHostname(or ip address) Text\n");
-		exit(1);
-	}
-	
-	
-	if ((host = gethostbyname(argv[1])) == NULL){
-		perror("gethostbyname");
-		exit(1);
-	}
-	
-	memset(buf, 0, sizeof(buf));
-	sprintf(buf, "%s", argv[2]);
-	
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-		perror("socket");
-		exit(1);
-	}
-	
+    // 将 "127.0.0.1" 替换为服务器的实际IP地址
+    if (inet_pton(AF_INET, "192.168.116.162", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported\n");
+        return;
+    }
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(port);
-	serv_addr.sin_addr = *((struct in_addr *)host->h_addr);
-	
-	
-	if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(struct sockaddr)) == -1){
-		perror("send");
-		exit(1);
-	}
-	close(sockfd);
-	exit(0);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed\n");
+        return;
+    }
+
+    send(sock, command, strlen(command), 0);
+    printf("Message sent: %s\n", command);
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+
+    close(sock);
+}
+// 检查输入的命令是否合法
+void is_valid_command(const char *command) {
+    // 检查是否是 "ON", "OFF", "SILENT"
+    if (strcmp(command, "ON") == 0 || strcmp(command, "OFF") == 0 || strcmp(command, "SILENT") == 0) {
+        sendCommand(command);
+    }else printf("Command Error\n");
+    // 检查是否是 "PERIODIC <ms>" 格式
+    if (strncmp(command, "PERIODIC ", 9) == 0) {
+        const char *ptr = command + 9;
+        while (*ptr) {
+            if (!isdigit(*ptr)) {
+                    printf("Command Error\n");
+                return ;
+            }
+            ptr++;
+        }
+        sendCommand(command);
+    }
+    return ;
+}
+int main() {
+    for(;;)
+    {
+    is_valid_command("ON");
+    sleep(2);
+    is_valid_command("PERIODIC 3000");
+    sleep(2);
+    is_valid_command("abc");
+    sleep(2);
+    is_valid_command("SILENT");
+    sleep(4);
+    }
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,60 +1,117 @@
-/*server.c*/
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
-#include <netinet/in.h>
-#define PORT	4321
-#define BUFFER_SIZE		1024
-#define MAX_QUE_CONN_NM  5
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdbool.h>
 
-int main(){
-	struct sockaddr_in server_sockaddr,client_sockaddr;
-	int sin_size,recvbytes;
-	int sockfd, client_fd;
-	char buf[BUFFER_SIZE];
-	
+#define PORT 12345
 
-	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){
-		perror("socket");
-		exit(1);
-	}
-	printf("Socket id = %d\n",sockfd);
-	
-	
-	server_sockaddr.sin_family = AF_INET;
-	server_sockaddr.sin_port = htons(PORT);
-	server_sockaddr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(server_sockaddr.sin_zero), 8);
-	
-	int i = 1;
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i));
-	
-	
-	if (bind(sockfd, (struct sockaddr *)&server_sockaddr,sizeof(struct sockaddr)) == -1)
-	{
-		perror("bind");
-		exit(1);
-	}
-	printf("Bind success!\n");
-	
+bool running = true;
+bool buzzer_on = false;
+int period = 1000;  // 默认周期为1000毫秒（1秒）
 
-	if (listen(sockfd, MAX_QUE_CONN_NM) == -1){
-		perror("listen");
-		exit(1);
-	}
-	printf("Listening....\n");
-	
+void *buzzerThread(void *arg) {
+    while (running) {
+        if (buzzer_on) {
+            printf("Buzzer ON\n");
+        } else {
+            printf("Buzzer OFF\n");
+        }
+        usleep(period * 1000);
+    }
+    return NULL;
+}
 
-	memset(buf, 0, sizeof(buf));
-	if((recvbytes = recv(client_fd, buf, BUFFER_SIZE, 0)) == -1){
-		perror("recv");
-		exit(1);
-	}
-	printf("Received a message: %s\n", buf);
-	close(sockfd);
-	exit(0);
+void handleClient(int new_socket) {
+    char buffer[1024] = {0};
+    int valread;
+
+    while ((valread = read(new_socket, buffer, 1024)) > 0) {
+        buffer[valread] = '\0';
+        printf("Received: %s\n", buffer);
+
+        // 处理蜂鸣器控制命令
+        if (strcmp(buffer, "ON") == 0) {
+            buzzer_on = true;
+            printf("Buzzer ON\n");
+        } else if (strcmp(buffer, "OFF") == 0) {
+            buzzer_on = false;
+            printf("Buzzer OFF\n");
+        } else if (strncmp(buffer, "PERIODIC ", 9) == 0) {
+            sscanf(buffer + 9, "%d", &period);
+            buzzer_on = true;
+            printf("Buzzer PERIODIC with period %d ms\n", period);
+        } else if (strcmp(buffer, "SILENT") == 0) {
+            buzzer_on = false;
+            printf("Buzzer SILENT\n");
+        } else {
+            printf("Invalid command: %s\n", buffer);
+        }
+
+        send(new_socket, "Command received", strlen("Command received"), 0);
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    if (valread == 0) {
+        printf("Client disconnected\n");
+    } else {
+        perror("Read error");
+    }
+}
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    pthread_t buzzer_thread;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // 创建一个线程来模拟蜂鸣器的周期性响和停
+    pthread_create(&buzzer_thread, NULL, buzzerThread, NULL);
+
+    while ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) >= 0) {
+        printf("Client connected\n");
+        handleClient(new_socket);
+        close(new_socket);
+    }
+
+    if (new_socket < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 结束程序时停止蜂鸣器线程
+    running = false;
+    pthread_join(buzzer_thread, NULL);
+
+    close(server_fd);
+    return 0;
 }
